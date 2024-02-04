52d9229e93976af4814d3503d3f75054acf17101
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-02-04 13:26:21 -0300

    Implementando middleware que  registra o tempo de duração de cada requisição em milisegundos

--- Modulo4/MinimalAPi_Example/Middlewares/RestMiddleware.cs
+++ Modulo4/MinimalAPi_Example/Middlewares/RestMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace MinimalAPi_Example.Middlewares.RestMiddleware;
+﻿using System.Diagnostics;
+
+namespace MinimalAPi_Example.Middlewares.RestMiddleware;
 
 public class RestMiddleware
 {
@@ -15,5 +17,29 @@ public class RestMiddleware
         await context.Response.WriteAsync($"Info Custom Middleware: {DateTime.Now} - IP: {context.Connection.RemoteIpAddress}\n");
 
         await _next(context);
+    }
+}
+
+public class RestMiddlewareDuration
+{
+    private readonly RequestDelegate _next;
+    public RestMiddlewareDuration(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.ContentType = "text/html; charset=utf-8";
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        await _next(context);
+
+        stopwatch.Stop();
+        var duracao = stopwatch.ElapsedMilliseconds;
+        await context.Response.WriteAsync($"<p>Duração da Requisição: {duracao} ms</p>");
+
     }
 }
--- Modulo4/MinimalAPi_Example/Program.cs
+++ Modulo4/MinimalAPi_Example/Program.cs
@@ -4,6 +4,7 @@ using MinimalAPi_Example.Middlewares.RestMiddleware;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseMiddleware<RestMiddlewareDuration>();
 app.UseMiddleware<RestMiddleware>();
 
 app.Run(async (context) =>
