86a151bdf9bbedd7fcbafc0835f9521107ed1199
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-02-25 23:37:53 -0300

    Adicionando o modelo User, criando as views e a migration

---
+++ MvcMovie/Data/Security/PasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcMovie.Data.Security;
+
+public class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        using (var sha256 = System.Security.Cryptography.SHA256.Create())
+        {
+            var hashedBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            return hash;
+        }
+        
+    }
+
+    public static bool VerifyPassword(string password, string hash)
+    {
+        return HashPassword(password) == hash;
+    }
+}
